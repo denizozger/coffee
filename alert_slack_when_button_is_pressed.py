c68578725e62390ae46dca1e85befca0f58c918d
Deniz Ozger <denizozger@gmail.com>
2016-02-28 18:01:22 +0000

    Added the buzzer

--- alert_slack_when_button_is_pressed.py
+++ alert_slack_when_button_is_pressed.py
@@ -19,31 +19,65 @@ GPIO.setup(RED, GPIO.OUT)
 GPIO.setup(YELLOW, GPIO.OUT)
 GPIO.setup(GREEN, GPIO.OUT)
 
-# Button Setup
+# Button setup
 ButtonPin = 25  # Set pin 25 as an input pin
 GPIO.setup(ButtonPin, GPIO.IN)
 
-# Default state is red
-GPIO.output(RED, GPIO.HIGH)
+# Buzzer setup
+BUZZER = 22
+GPIO.setup(BUZZER, GPIO.OUT)
 
-while True:
-	if GPIO.input(ButtonPin) == False:  # If the button is pressed, ButtonPin will be "false"
-		print('Button Pressed')
-		GPIO.output(RED, GPIO.LOW)
-		GPIO.output(YELLOW, GPIO.HIGH)
+def main():
+	reset_devices()
+	GPIO.output(RED, GPIO.HIGH)  # Default state is red
+	while True:
+		if GPIO.input(ButtonPin) == False:  # If the button is pressed, ButtonPin will be "false"
+			print('Button Pressed')
+			GPIO.output(RED, GPIO.LOW)
+			GPIO.output(YELLOW, GPIO.HIGH)
 
-		message = { 'text': 'Ready!'}
-		response = requests.post(url, data=message, allow_redirects=True)
-		if response.status_code == 200:
-			GPIO.output(YELLOW, GPIO.LOW)
-			GPIO.output(GREEN, GPIO.HIGH)
-			time.sleep(2400)  # Coffee stays fresh for 40 minutes - I think
+			message = { 'text': 'Ready!'}
+			response = requests.post(url, json=message, allow_redirects=True)
+			print(response.status_code)
+			if response.status_code == 200:
+				# buzz()
+				GPIO.output(YELLOW, GPIO.LOW)
+				GPIO.output(GREEN, GPIO.HIGH)
+				time.sleep(5)  # Coffee stays fresh for 40 minutes - I think
+			else:
+				GPIO.output(YELLOW, GPIO.LOW)
+				for x in range(0, 3):
+					GPIO.output(RED, GPIO.HIGH)
+					time.sleep(0.5)
 		else:
-			GPIO.output(YELLOW, GPIO.LOW)
-			for x in range(0, 3):
-				GPIO.output(RED, GPIO.HIGH)
-				time.sleep(0.5)
-	else:
-		os.system('clear') # Clears the screen
-		print('Waiting for you to press a button')
-		time.sleep(0.5)
+			os.system('clear') # Clears the screen
+			reset_devices()
+			GPIO.output(RED, GPIO.HIGH)
+			print('Waiting for you to press a button')
+			time.sleep(0.5)
+
+def buzz(duration: int=0.3):
+	GPIO.output(BUZZER, GPIO.HIGH)
+	time.sleep(duration)
+	GPIO.output(BUZZER, GPIO.LOW)
+
+def reset_devices():
+	GPIO.output(BUZZER, GPIO.LOW)
+	GPIO.output(RED, GPIO.LOW)
+	GPIO.output(YELLOW, GPIO.LOW)
+	GPIO.output(GREEN, GPIO.LOW)
+
+
+try:
+    main()
+except Exception as e:
+    print(e)
+    GPIO.output(BUZZER, GPIO.LOW)
+    GPIO.output(RED, GPIO.LOW)
+    GPIO.output(YELLOW, GPIO.LOW)
+    GPIO.output(GREEN, GPIO.LOW)
+    GPIO.cleanup()
+    try:
+        sys.exit(0)
+    except SystemExit:
+        os._exit(0)
